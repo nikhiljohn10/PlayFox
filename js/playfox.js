e0496d1727cf2b02bdaf07fb08eef3f0da9bc2a3
Nikhil John <nikhiljohn1010@gmail.com>
2015-04-07 04:05:30 +0530

    Fox trail complete

--- js/playfox.js
+++ js/playfox.js
@@ -1,77 +1,55 @@
-/* 
 
-Fox Trail
-
+/*
+    Fox Trail
 */
 
+$(document).ready(function() {
+    var pawId = 0,
+        lastLeft = 0,
+        lastTop = 0;
 
+    function checkSpeed(interval, callback) {
+        var t1 = (new Date).getTime();
+        return function() {
+            var t2 = (new Date).getTime();
+            t2 - t1 >= interval && (t1 = t2, callback.apply(null, arguments))
+        }
+    }
 
-var set, thread, x1,x2,y1,y2,c;
-var timeWalk;
-var Trail = [];
-var trailBox = document.getElementById('fox-trail'); 
-var hook = document.getElementById('mouse-hook'); 
+    function findAngle(x1, y1, x2, y2) {
+        lastTop = y2;
+        lastLeft = x2;
+        var distX = x2 - x1;
+        var distY = y2 - y1;
+        var rad = Math.atan2(distY, distX);
+        var deg = (rad * (180 / Math.PI)) + 90;
+        return deg;
+    }
 
-var Paw = function (X,Y,angle=0) {
-	this.img = document.createElement("img");
-	this.img.src = "images/fox-paw.png";
-	this.img.style.position = "absolute";
-	this.img.style.left = X+"px";
-	this.img.style.top = Y+"px";
-	this.img.style.display = "none";
-	this.img.style.transform = "rotate("+angle+"deg)";
-	trailBox.appendChild(this.img);
-	$(this.img).fadeIn(500,function () {
-		setTimeout(function () {
-			var p = Trail.shift();
-			$(p.img).fadeOut(1000,function () {
-				this.parentNode.removeChild(this);
-			});
-		},1000);
-	});
-};
-
-function addPath (e, deg=0) {
-	Trail.push(new Paw(e.pageX,e.pageY,deg));
-}
-
-document.addEventListener('click',addPath);
-
-$("body").mousemove(function(e) {
-    if (!set) {
-        x1 = e.pageX, //set starting mouse x
-        y1 = e.pageY, //set starting mouse y
-        set = true;
-        // timeWalk = setInterval(stepNext(e),500);
-    } 
-    clearTimeout(thread);
-    thread = setTimeout(callback.bind(this, e), 100);
+    $(document).mousemove(checkSpeed(150, function(event) {
+        if ($(window).width() >= 768) {
+            var left = event.pageX;
+            var top = event.pageY;
+            var angle = findAngle(lastLeft, lastTop, left, top);
+            if ((angle < 0 && angle > -90) || (angle > 90 && angle < 180)) {
+                if (pawId % 2 == 0) {
+                    top += 20;
+                    left -= 20;
+                } else {
+                };
+            } else {
+                if (pawId % 2 == 0) {
+                    top += 20;
+                    left += 20;
+                }
+            }
+            $('body').append('<img src="images/fox-paw20.png" class="paw" id="paw' + pawId + '" style="top: ' + top + 'px; left: ' + left + 'px; transform: rotate(' + angle + 'deg);" />');
+            setTimeout(function() {
+                $('.paw').each(function() {
+                    $(this).css('opacity') < '0.1' && $(this).remove()
+                })
+            }, 1000);
+            pawId++;
+        }
+    }));
 });
-
-function stepNext (e) {
-	log('Paw @ ('+x1+','+y1+')');
-	x2 = e.pageX; //new X
-    y2 = e.pageY; //new Y
-	addPath(x1,y1,getAngle(x1, y1, x2, y2));
-	set = false;
-}
-
-function getAngle (x1, y1, x2, y2) {
-    var distY = y2-y1; //opposite
-    var distX = x2-x1; //adjacent
-    var rad = Math.atan2(distY, distX);
-    var deg = (rad * (180 / Math.PI))+90;
-    return deg; //return angle in degrees
-}
-
-function callback(e) {
-   	clearInterval(timeWalk);
-    set = false;
-}
-
-log = function (string) {
-	console.log(string);	
-}
-
-
-// enabled = !(window.ontouchstart !== undefined);
